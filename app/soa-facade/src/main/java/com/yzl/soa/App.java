8b222581966da4c189712238a06d6e862f6e14c2
yzlxlf <1634234715@qq.com>
2017-02-26 14:36:50 +0800

    SOA框架搭建

---
+++ app/soa-facade/src/main/java/com/yzl/soa/App.java
@@ -0,0 +1,13 @@
+package com.yzl.soa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
