dada9fd8a409865d27b1a7fa75c69a260f579f8f
yzlxlf <1634234715@qq.com>
2017-02-26 15:51:03 +0800

    SOA 添加assembly模块

---
+++ app/soa-util/src/main/java/com/yzl/soa/SOATestMain.java
@@ -0,0 +1,37 @@
+/**
+ * All rights Reserved, Designed By https://github.com/yzlxlf/soa
+ *
+ * @Title: ${FILE_NAME}
+ * @Package com.yzl.soa
+ * @Description: ${TODO}(用一句话描述该文件做什么)
+ * @author: yuzhenlong_俞真龙
+ * @version V1.0
+ * @Copyright: ${year} https://github.com/yzlxlf/soa Inc. All rights reserved.
+ */
+package com.yzl.soa;
+
+/**
+ * @author yuzhenlong
+ * @version $Id SOATestMain.java, v 1.0 2017年02月26日 14时58分 yuzhenlong Exp $
+ */
+public class SOATestMain {
+
+    //八大基础类型
+    byte aByte;
+    int anInt;
+    char aChar;
+    double aDouble;
+    float aFloat;
+    long aLong;
+    short aShort;
+    boolean aBoolean;
+
+    public static void main(String[] args) {
+
+    }
+
+
+    class Inner {}
+
+    public void bar() { new Inner(); }
+}
