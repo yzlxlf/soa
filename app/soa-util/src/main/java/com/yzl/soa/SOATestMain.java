5d4cbf9937aa87bae25cfa9dde60b1ab679dd8fe
yzlxlf <1634234715@qq.com>
2017-02-27 09:41:50 +0800

    SOA 添加web模块

--- app/soa-util/src/main/java/com/yzl/soa/SOATestMain.java
+++ app/soa-util/src/main/java/com/yzl/soa/SOATestMain.java
@@ -27,7 +27,9 @@ public class SOATestMain {
     boolean aBoolean;
 
     public static void main(String[] args) {
-
+        Integer a=100,b=100,c=-129,d=-129;
+        System.out.println(a==b);
+        System.out.println(c==d);
     }
 
 
